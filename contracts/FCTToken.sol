8b28a35a0e1f35eca271224d057f65ecdfdf58c3
Steve Boyer <stephen.a.boyer@gmail.com>
2022-01-31 14:17:05 -0600

    Deploy contract to local blockchain

--- contracts/FCTToken.sol
+++ contracts/FCTToken.sol
@@ -7,6 +7,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FCTToken is ERC20 {
 
+    // Initialize the smart contract parameters name and symbol with values that will be passed to it when it is deployed
+      // Parameter 1 = name = Faucet
+      // Parameter 1 = symbol = FCT
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         _mint(msg.sender, 100000 * (10 ** 18));
     }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,30 +3,31 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
+
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log(
+    "Deploying contracts with the account:",
+    deployer.address
+  );
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const FCTToken = await hre.ethers.getContractFactory("FCTToken");
+  const fctToken = await FCTToken.deploy("FCTToken", "TKN");
 
-  await greeter.deployed();
+  await fctToken.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Token deployed to:", fctToken.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main()
   .then(() => process.exit(0))
-  .catch((error) => {
+  .catch(error => {
     console.error(error);
     process.exit(1);
   });
