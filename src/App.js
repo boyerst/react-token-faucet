f069dc1661148aff06cef1e238b3d15037af52e7
Steve Boyer <stephen.a.boyer@gmail.com>
2022-02-01 19:29:44 -0600

    Debugged for Netlify deploy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import FCTToken from './artifacts/contracts/FCTToken.sol/FCTToken.json'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container, Row, Col } from 'react-bootstrap'
 import Faucet from './components/Faucet.js'
 import TokenSend from './components/TokenSend.js'
+import FCTToken from './artifacts/contracts/FCTToken.sol/FCTToken.json'
 
 
 
